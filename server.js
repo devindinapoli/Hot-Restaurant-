aa043692ae6f95b946dbe2bcb202e3a486cdbbd5
Devin DiNapoli <devindinapoli@gmail.com>
2018-03-30 10:11:02 -0400

    added server

---
+++ server.js
@@ -0,0 +1,81 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname, ""));
+});
+
+app.get("/tables", function(req,res){
+    res.sendFile(path.join(__dirname, ""));
+});
+
+app.get("/reserve", function(req,res){
+    res.sendFile(path.join(__dirname, ""));
+});
+
+//Fill with dummy data later 
+var tables = [
+    {
+        name: "Devin",
+        number: "910-550-7665",
+        email: "devindinapoli@gmail.com",
+        id: "The Dude"
+    },
+    {
+        name: "Devin",
+        number: "910-550-7665",
+        email: "devindinapoli@gmail.com",
+        id: "The Dude"
+    },
+    {
+        name: "Devin",
+        number: "910-550-7665",
+        email: "devindinapoli@gmail.com",
+        id: "The Dude"
+    },
+    {
+        name: "Devin",
+        number: "910-550-7665",
+        email: "devindinapoli@gmail.com",
+        id: "The Dude"
+    },
+    {
+        name: "Devin",
+        number: "910-550-7665",
+        email: "devindinapoli@gmail.com",
+        id: "The Dude"
+    },
+];
+
+app.get("/api/tables", function(req,res){
+    res.json(tables);
+});
+
+app.post("/api/reserve", function(req,res){
+    var newRes = req.body;
+
+    tables.push(newRes);
+
+    var isFull;
+
+    if(tables.length === 5) {
+        isFull = true
+    }
+    else{
+        isFull = false;
+    }
+    res.json(isFull);
+});
+
+app.listen(PORT, function(){
+    console.log("Listening on PORT " + PORT);
+});
